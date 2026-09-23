5936d23835d38e30c59d5cd376e868f2f828f5a6
denernaresidasilva <denernaresidasilva2@gmail.com>
2026-09-23 10:03:06 -0300

    Libera ambiente dev no CORS da API

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://kalend.tech', 'https://www.kalend.tech'],
+    origin: [
+      'https://kalend.tech',
+      'https://www.kalend.tech',
+      'https://dev.kalend.tech',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
